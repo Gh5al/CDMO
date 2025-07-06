be9e599ea0a2149706d633434a03c5f2b67518f3
Daniele Santini <danysan95@gmail.com>
2025-07-06 21:58:38 +0200

    Add CSV

---
+++ result/res_to_csv.py
@@ -0,0 +1,21 @@
+from glob import glob
+import pandas as pd
+import os
+import json
+
+for technique in ["CP", "SMT", "MIP"]:
+    print(f"Reading {technique}")
+    filepaths = glob(f'../res/{technique}/*.json') # get list of json files in folder
+    df = pd.DataFrame(columns=["instance", "variant", "obj", "optimal", "time", "sol"])
+
+    for f in filepaths:
+        with open(f) as json_data:
+            filename = os.path.basename(f).rsplit('.', 1)[0] # extract filename without extension
+            instance_result = json.load(json_data)
+            for variant in instance_result:
+                row = instance_result[variant]
+                row["instance"] = filename
+                row["variant"] = variant
+                print(row)
+                df = df.append(row, ignore_index=True)
+        df.to_csv(f'../res/{technique}.csv', index=False)
