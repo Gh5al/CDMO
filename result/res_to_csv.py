5aedcbafc5c8b135c5e9eac46ba8cd7f8491874e
Daniele Santini <danysan95@gmail.com>
2025-07-06 22:55:46 +0200

    Update CSV

--- result/res_to_csv.py
+++ result/res_to_csv.py
@@ -6,21 +6,19 @@ import json
 for technique in ["CP", "SMT", "MIP"]:
     print(f"Reading {technique}")
     filepaths = glob(f'../res/{technique}/*.json') # get list of json files in folder
-    df = pd.DataFrame(columns=["instance", "variant", "obj", "optimal", "time", "sol"])
+    df = pd.DataFrame()
 
     for f in filepaths:
         with open(f) as json_data:
             filename = os.path.basename(f).rsplit('.', 1)[0] # extract filename without extension
             instance_result = json.load(json_data)
-            for variant in instance_result:
-                row = pd.Series(instance_result[variant])
-                row["instance"] = int(filename)
-                row["variant"] = variant
-                #print(row)
-                df = pd.concat([
-                    df,
-                    row.to_frame().T
-                ], ignore_index=True)
-    df.astype({
-            "instance":int, "variant":str, "obj":int, "optimal":bool, "time":int, "sol":str
-        }).sort_values(["instance","variant"]).to_csv(f'{technique}.csv', index=False)
+            row_dict = {"instance":int(filename)}
+            for variant in instance_result.keys():
+                row_dict[variant] = instance_result[variant]["obj"]
+                #row[f'{variant}_optimal'] = instance_result[variant]["optimal"]
+            row_df = pd.DataFrame([row_dict])
+            print(row_df)
+            df = pd.concat([df, row_df], ignore_index=True)
+    df.astype({"instance":int })\
+        .sort_values(["instance"])\
+        .to_csv(f'{technique}.csv', index=False)
